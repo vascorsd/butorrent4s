4ccbb17c5d15c52af948bfb4122741adce0de739
Vasco Dias <13673+vascorsd@users.noreply.github.com>
2024-05-10 20:48:30 +0100

    scalafmt complains about dialet being mandatory

--- src/main.scala
+++ src/main.scala
@@ -1,11 +1,12 @@
 import cats.effect.*
 
-
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     if (args.nonEmpty) {
-      IO.println("I did it!").as(ExitCode.Success)
+      IO.println("I did it!")
+        .as(ExitCode.Success)
     } else {
-      IO.println("Didn't do it").as(ExitCode(-1))
+      IO.println("Didn't do it")
+        .as(ExitCode(-1))
     }
 }
