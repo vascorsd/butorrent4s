1efb494f1d06b239d7f1d330ed63871c9bd0225a
Vasco Dias <m+git@vascorsd.com>
2024-05-24 19:24:21 +0100

    tests: add some for the integer parsing

--- test/decoder.test.scala
+++ test/decoder.test.scala
@@ -1,5 +1,7 @@
+package butorrent4s
+
 class DecoderTests extends munit.FunSuite {
-  test("parseByteString failure inputs") {
+  test("parseByteString ❌") {
     assert(parseByteString("".toList) == None)
     assert(parseByteString(":".toList) == None)
     assert(parseByteString("s".toList) == None)
@@ -16,10 +18,33 @@ class DecoderTests extends munit.FunSuite {
     assert(parseByteString("01:".toList) == None)
   }
 
-  test("parseByteString success inputs") {
+  test("parseByteString ✔") {
     assert(parseByteString("1:s".toList) == Some("s"))
     assert(parseByteString("0:".toList) == Some(""))
     assert(parseByteString("2:ss".toList) == Some("ss"))
     assert(parseByteString("10:ssssssssss".toList) == Some("ssssssssss"))
+  }
+
+  test("parseInteger ❌") {
+    assert(parseInteger("i010e".toList) == None)
+    assert(parseInteger("iss1e".toList) == None)
+    assert(parseInteger("i5ie".toList) == None)
+    assert(parseInteger("i0101010101010000000000000".toList) == None)
+
+    assert(parseInteger("i-0e".toList) == None)
+
+    assert(parseInteger("i-s5e".toList) == None)
+    assert(parseInteger("i-i1e".toList) == None)
+  }
+
+  test("parseInteger ✔") {
+    assert(parseInteger("i1e".toList) == Some(1))
+    assert(parseInteger("i10e".toList) == Some(10))
+    assert(parseInteger("i9999999999e:".toList) == Some(9999999999L))
+
+    assert(parseInteger("i0e".toList) == Some(0))
+
+    assert(parseInteger("i-50e".toList) == Some(-50))
+    assert(parseInteger("i-9999999999e:".toList) == Some(-9999999999L))
   }
 }
